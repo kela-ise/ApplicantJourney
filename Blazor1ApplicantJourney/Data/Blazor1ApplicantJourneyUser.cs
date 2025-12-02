3be721a90dd1c9e18f4c0b332e97eed1ca9c8707
Kela <kelaise2@gmail.com>
2025-12-01 22:35:13 -0500

    Create a new Blazor UI to fix Scaffolding issues

--- Blazor1ApplicantJourney/Data/Blazor1ApplicantJourneyUser.cs
+++ Blazor1ApplicantJourney/Data/Blazor1ApplicantJourneyUser.cs
@@ -5,5 +5,6 @@ namespace Blazor1ApplicantJourney.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class Blazor1ApplicantJourneyUser : IdentityUser
     {
+
     }
 }
